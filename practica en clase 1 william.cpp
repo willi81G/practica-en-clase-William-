62a869566cec262ef783e31926c0107fd40748ff
willi81G <wolorosog@miumg.edu.gt>
2024-02-17 12:59:04 -0600

    william
    suma de william

---
+++ practica en clase 1 william.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	int a = 4;  // declaramos e inicializamos una variable entera 
+	int b = 7;  // declaramos e inicializamos una variable entera 
+	
+	int suma = a + b;  // realizamos la operacion
+	
+	// imprimimos el resultado en pantalla 
+	cout<<" El resultado de la suma es: "<<suma<<endl;
+	return 0;
+}
